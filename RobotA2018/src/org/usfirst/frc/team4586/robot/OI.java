bd2c402c9004bc3e51ac2025eb3e8bc96a4203e7
אני <@Lenovo-DriverPC>
2018-02-28 19:54:13 +0200

    zikpa and hands

--- RobotA2018/src/org/usfirst/frc/team4586/robot/OI.java
+++ RobotA2018/src/org/usfirst/frc/team4586/robot/OI.java
@@ -18,6 +18,7 @@ import org.usfirst.frc.team4586.robot.commands.Invert;
 import org.usfirst.frc.team4586.robot.commands.LiftToFloor;
 import org.usfirst.frc.team4586.robot.commands.LiftToSwitch;
 import org.usfirst.frc.team4586.robot.commands.OpenPlatforms;
+import org.usfirst.frc.team4586.robot.commands.OpenZikpaAndHands;
 import org.usfirst.frc.team4586.robot.commands.OpendShloplop;
 import org.usfirst.frc.team4586.robot.commands.ResetEncoder;
 import org.usfirst.frc.team4586.robot.commands.StopAllMotors;
@@ -44,6 +45,7 @@ public class OI {
 	public JoystickButton resetEncoder;
 	public JoystickButton cubePusher;
 	public JoystickButton noDelayCatch;
+	public JoystickButton openBothThings;
 	
 	//operator
 	public Joystick joystickOpertor;
@@ -58,46 +60,49 @@ public class OI {
 	public JoystickButton toggleShloplop;
 	public JoystickButton unlockElevator;
 	
+	
 	public OI()
 	{
 		//buttons left -> 2,7
-		joystickDriver = new Joystick(0);
-		stopAllMotors = new JoystickButton(joystickDriver , 9);
-		//invert = new JoystickButton(joystickDriver , 4);
-		resetEncoder = new JoystickButton(joystickDriver , 8);
-		cubePusher = new JoystickButton(joystickDriver , 3);
-		catchCube = new JoystickButton(joystickDriver , 5);
-		noDelayCatch = new JoystickButton(joystickDriver, 6);
+				joystickDriver = new Joystick(0);
+				stopAllMotors = new JoystickButton(joystickDriver , 9);
+				//invert = new JoystickButton(joystickDriver , 4);
+				resetEncoder = new JoystickButton(joystickDriver , 8);
+				cubePusher = new JoystickButton(joystickDriver , 3);
+				catchCube = new JoystickButton(joystickDriver , 5);
+				noDelayCatch = new JoystickButton(joystickDriver, 6);
+				openBothThings = new JoystickButton(joystickDriver, 1);
+				
+				joystickOpertor = new Joystick(1);
+				liftToFloor = new JoystickButton(joystickOpertor , 3);
+				liftToSwitch = new JoystickButton(joystickOpertor ,5);
+				climbR = new JoystickButton(joystickOpertor , 6);
+				openPlatform = new JoystickButton(joystickOpertor , 2);
+				switchCompressor = new JoystickButton(joystickOpertor, 7);
+				calibrateGyro = new JoystickButton(joystickOpertor , 8);
+				downRight = new JoystickButton(joystickOpertor, 4);
+				toggleShloplop = new JoystickButton(joystickOpertor, 1);
+				unlockElevator = new JoystickButton(joystickOpertor, 10);
+				
+				//driver commands
+				stopAllMotors.whenPressed(new StopAllMotors());
+				//invert.whenPressed(new Invert());
+				calibrateGyro.whenPressed(new CalibrateGyro());
+				resetEncoder.whenPressed(new ResetEncoder());
+				cubePusher.whenPressed(new CubePusher());
+				catchCube.whenPressed(new CatchCube());
+				noDelayCatch.whenPressed(new CubeCatcherNoDelay());
+				openBothThings.whenPressed(new OpenZikpaAndHands());
+				
+				//operator commands
+				liftToFloor.toggleWhenPressed(new LiftToFloor());
+				liftToSwitch.toggleWhenPressed(new LiftToSwitch());
+				climbR.whileHeld(new ClimbRight());
+				openPlatform.whenPressed(new OpenPlatforms());
+				switchCompressor.whenPressed(new SwitchCompressor());
+				downRight.whileHeld(new DownRight());
+				//toggleShloplop.whenPressed(new OpendShloplop());
+				unlockElevator.whenPressed(new UnlockElevator());
 		
-		
-		joystickOpertor = new Joystick(1);
-		liftToFloor = new JoystickButton(joystickOpertor , 3);
-		liftToSwitch = new JoystickButton(joystickOpertor ,5);
-		climbR = new JoystickButton(joystickOpertor , 6);
-		openPlatform = new JoystickButton(joystickOpertor , 2);
-		switchCompressor = new JoystickButton(joystickOpertor, 7);
-		calibrateGyro = new JoystickButton(joystickOpertor , 8);
-		downRight = new JoystickButton(joystickOpertor, 4);
-		toggleShloplop = new JoystickButton(joystickOpertor, 1);
-		unlockElevator = new JoystickButton(joystickOpertor, 10);
-		
-		//driver commands
-		stopAllMotors.whenPressed(new StopAllMotors());
-		//invert.whenPressed(new Invert());
-		calibrateGyro.whenPressed(new CalibrateGyro());
-		resetEncoder.whenPressed(new ResetEncoder());
-		cubePusher.whenPressed(new CubePusher());
-		catchCube.whenPressed(new CatchCube());
-		noDelayCatch.whenPressed(new CubeCatcherNoDelay());
-		
-		//operator commands
-		liftToFloor.toggleWhenPressed(new LiftToFloor());
-		liftToSwitch.toggleWhenPressed(new LiftToSwitch());
-		climbR.whileHeld(new ClimbRight());
-		openPlatform.whenPressed(new OpenPlatforms());
-		switchCompressor.whenPressed(new SwitchCompressor());
-		downRight.whileHeld(new DownRight());
-		toggleShloplop.whenPressed(new OpendShloplop());
-		unlockElevator.whenPressed(new UnlockElevator());
 	}
 }
